0abba5c1d96195b12e250fe1b474555b67bf0af6
fahmidzulf <fahmifikri13@gmail.com>
2017-02-13 16:02:05 +0700

    Views Anggota

--- application/models/m_anggota.php
+++ application/models/m_anggota.php
@@ -1,45 +1,45 @@
-  <?php
+<?php
 class M_anggota extends CI_Model{
-  private $table="anggota";
-  private $primary="nis";
+    private $table="anggota";
+    private $primary="nis";
 
-  function semua($limit=10, $offset=0, $order_column='',$oder_type='asc'){
-    if(empty($order_column) || empty($order_type))
-    $this->db->order_by($this->primary, 'asc');
+    function semua($limit=10,$offset=0,$order_column='',$order_type='asc'){
+        if(empty($order_column) || empty($order_type))
+            $this->db->order_by($this->primary,'asc');
+        else
+            $this->db->order_by($order_column,$order_type);
+        return $this->db->get($this->table,$limit,$offset);
+    }
 
-    else
-    $this->db->order_by($order_column, $order_type);
-    return $this->db->get($this->table, $limit, $offset);
-  }
+    function jumlah(){
+        return $this->db->count_all($this->table);
+    }
 
-  function jumlah(){
-    return $this->db->count_all($this->table);
-  }
-  function cek(){
-    $this->db->where($this->primary,$kode);
-    $query=$this->db->get($this->table);
+    function cek($kode){
+        $this->db->where($this->primary,$kode);
+        $query=$this->db->get($this->table);
 
-    return $query;
-  }
+        return $query;
+    }
 
-  function simpan($jenis){
-    $this->db->insert($this->primary, $kode);
-    return $this->db->insert_id();
-  }
+    function simpan($jenis){
+        $this->db->insert($this->table,$jenis);
+        return $this->db->insert_id();
+    }
 
-  function update($kode,$jenis){
-    $this->db->where($this->priay, $kode);
-    return $this->insert_id();
-  }
+    function update($kode,$jenis){
+        $this->db->where($this->primary,$kode);
+        $this->db->update($this->table,$jenis);
+    }
 
-  function hapus($kode){
-    $this->db->where($this->primary, $kode);
-    $this->db->where($this->table);
-  }
+    function hapus($kode){
+        $this->db->where($this->primary,$kode);
+        $this->db->delete($this->table);
+    }
 
-  function cari($cari){
-    $this->db->like($this->primary, $cari);
-    $this->db->or_like("nama", $cari);
-    return $this->db->get($this->table);
-  }
+    function cari($cari){
+        $this->db->like($this->primary,$cari);
+        $this->db->or_like("nama",$cari);
+        return $this->db->get($this->table);
+    }
 }
--- application/views/anggota/tambah.php
+++ application/views/anggota/tambah.php
@@ -1,4 +1,5 @@
 <!DOCTYPE html>
+<legend>Tambah Data Anggota</legend>
 <html>
   <head>
     <meta charset="utf-8">
@@ -15,7 +16,54 @@
 
   </head>
   <body>
-      <form class="form-horizantal" action="  "
+    <?php echo validation_errors();?>
+    <?php echo $message;?>
 
-  </body>
+    <form class="form-horizontal" action="" method="post" enctype="multipart/form-data">
+    <div class="form-group">
+        <label class="col-lg-2 control-label">Nis</label>
+        <div class="col-lg-5">
+            <input type="text" name="nis" class="form-control">
+        </div>
+    </div>
+    <div class="form-group">
+        <label class="col-lg-2 control-label">Nama</label>
+        <div class="col-lg-5">
+            <input type="text" name="nama" class="form-control">
+        </div>
+    </div>
+    <div class="form-group">
+        <label class="col-lg-2 control-label">Jenis Kelamin</label>
+        <div class="col-lg-5">
+            <select name="jk" class="form-control">
+                <option></option>
+                <option value="L">L</option>
+                <option value="P">P</option>
+            </select>
+        </div>
+    </div>
+    <div class="form-group">
+        <label class="col-lg-2 control-label">Tanggal Lahir</label>
+        <div class="col-lg-5">
+            <input type="text" name="ttl" id="tanggal" class="form-control">
+        </div>
+    </div>
+    <div class="form-group">
+        <label class="col-lg-2 control-label">Kelas</label>
+        <div class="col-lg-5">
+            <input type="text" name="kelas" class="form-control">
+        </div>
+    </div>
+    <div class="form-group">
+        <label class="col-lg-2 control-label">Image</label>
+        <div class="col-lg-5">
+            <input type="file" name="gambar" class="form-control">
+        </div>
+    </div>
+    <div class="form-group well">
+        <button class="btn btn-primary"> Simpan</button>
+        <a href="<?php echo site_url('anggota');?>" class="btn btn-default">Kembali</a>
+    </div>
+</form>
+</body>
 </html>
