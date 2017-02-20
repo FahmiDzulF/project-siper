43ffb535865208f2b114b702f6b6d2816d4186cc
fahmidzulf <fahmifikri13@gmail.com>
2017-02-20 14:03:51 +0700

    Models, SQL

--- application/controllers/buku.php
+++ application/controllers/buku.php
@@ -31,6 +31,124 @@ class Buku extends CI_Controller{
       $this->pagination->initialize($config);
       $data['pagination']=$this->pagination->create_links();
 
-      if()
-}
-}
+      if($this->uri->segment(3)=="delete_success")
+        $data['message']="<div class='alert alert-success'>Data berhasil dihapus</div>";
+          else if($this->uri->segment(3)=="add_success")
+        $data['message']="<div class='alert alert-success'>Data berhasil disimpan</div>";
+          else
+        $data['message']='';
+        $this->template->display('buku/index', $data);
+      }
+
+      function tambah(){
+        $data['title']="Tambah Buku";
+        $this->set_rules();
+        if($this->form_validation->run()==true){
+          $kode=$this->input->post('kode');
+          $cek=$this->m_buku->cek($kode);
+
+          if($cek->num_rows()>0){
+            $data['message']="<div class='alert alert-danger'>Kode Buku suda ada</div>";
+            $this->template->display('buku/tambah',$data);
+          }else{
+
+            $config['upload_path'] = './assets/img/';
+            $config['allowed_types'] = 'gif|jpg|png';
+            $config['max_size']	= '1000';
+            $config['max_width']  = '2000';
+            $config['max_height']  = '1024';
+
+                            $this->upload->initialize($config);
+                            if(!$this->upload->do_upload('gambar')){
+                                $gambar="";
+                            }else{
+                                $gambar=$this->upload->file_name;
+                            }
+
+                            $info=array(
+                                'kode_buku'=>$this->input->post('kode'),
+                                'judul'=>$this->input->post('judul'),
+                                'pengarang'=>$this->input->post('pengarang'),
+                                'klasifikasi'=>$this->input->post('klasifikasi'),
+                                'image'=>$gambar
+                            );
+                            $this->m_buku->simpan($info);
+                            redirect('buku/index/add_success');
+
+                        }
+                    }else{
+                        $data['message']="";
+                        $this->template->display('buku/tambah',$data);
+                    }
+                }
+
+                function edit($id){
+                  $data['title']="Edit data Buku";
+                  $this->_set_rules();
+                  if($this->form_validation->run()==true){
+                      $kode=$this->input->post('kode');
+
+                      //setting konfiguras upload image
+                      $config['upload_path'] = './assets/img/';
+          	    $config['allowed_types'] = 'gif|jpg|png';
+                      $config['max_size']	= '1000';
+          	    $config['max_width']  = '2000';
+          	    $config['max_height']  = '1024';
+
+                      $this->upload->initialize($config);
+                      if(!$this->upload->do_upload('gambar')){
+                          $gambar="";
+                      }else{
+                          $gambar=$this->upload->file_name;
+                      }
+
+                      $info=array(
+                          'judul'=>$this->input->post('judul'),
+                          'pengarang'=>$this->input->post('pengarang'),
+                          'klasifikasi'=>$this->input->post('klasifikasi'),
+                          'image'=>$gambar
+                      );
+                      $this->m_buku->update($kode,$info);
+
+                      $data['buku']=$this->m_buku->cek($id)->row_array();
+                      $data['message']="<div class='alert alert-success'>Data berhasil diupdate</div>";
+                      $this->template->display('buku/edit',$data);
+                  }else{
+                      $data['message']="";
+                      $data['buku']=$this->m_buku->cek($id)->row_array();
+                      $this->template->display('buku/edit',$data);
+                  }
+              }
+
+              function hapus(){
+                  $kode=$this->input->post('kode');
+                  $detail=$this->m_buku->cek($kode)->result();
+          	foreach($detail as $det):
+          	    unlink("assets/img/".$det->image);
+          	endforeach;
+                  $this->m_buku->hapus($kode);
+              }
+
+              function cari(){
+                  $data['title']="Pencairan";
+                  $cari=$this->input->post('cari');
+                  $cek=$this->m_buku->cari($cari);
+                  if($cek->num_rows()>0){
+                      $data['message']="";
+                      $data['buku']=$cek->result();
+                      $this->template->display('buku/cari',$data);
+                  }else{
+                      $data['message']="<div class='alert alert-success'>Data tidak ditemukan</div>";
+                      $data['buku']=$cek->result();
+                      $this->template->display('buku/cari',$data);
+                  }
+              }
+
+              function _set_rules(){
+                  $this->form_validation->set_rules('kode','Kode Buku','required|max_length[5]');
+                  $this->form_validation->set_rules('judul','Judul Buku','required|max_length[100]');
+                  $this->form_validation->set_rules('pengarang','Pengarang','required|max_length[50]');
+                  $this->form_validation->set_rules('klasifikasi','Klasifikasi','required|max_length[25]');
+                  $this->form_validation->set_error_delimiters("<div class='alert alert-danger'>","</div>");
+              }
+          }
---
+++ db_perpustakaan.sql
@@ -0,0 +1,119 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Feb 20, 2017 at 08:02 AM
+-- Server version: 10.1.19-MariaDB
+-- PHP Version: 5.6.28
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_perpustakaan`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `anggota`
+--
+
+CREATE TABLE `anggota` (
+  `nis` varchar(10) NOT NULL,
+  `nama` varchar(50) DEFAULT NULL,
+  `jk` varchar(2) DEFAULT NULL,
+  `ttl` date DEFAULT NULL,
+  `kelas` varchar(10) DEFAULT NULL,
+  `image` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `buku`
+--
+
+CREATE TABLE `buku` (
+  `kode_buku` varchar(5) NOT NULL,
+  `judul` varchar(100) DEFAULT NULL,
+  `pengarang` varchar(50) DEFAULT NULL,
+  `klasifikasi` varchar(25) DEFAULT NULL,
+  `image` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pengembalian`
+--
+
+CREATE TABLE `pengembalian` (
+  `id_transaksi` varchar(12) NOT NULL,
+  `tgl_pengembalian` date NOT NULL,
+  `denda` varchar(2) NOT NULL,
+  `nominal` double NOT NULL,
+  `id_petugas` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `petugas`
+--
+
+CREATE TABLE `petugas` (
+  `id_petugas` int(11) NOT NULL,
+  `user` varchar(45) DEFAULT NULL,
+  `password` text
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `petugas`
+--
+
+INSERT INTO `petugas` (`id_petugas`, `user`, `password`) VALUES
+(3, 'admin', '21232f297a57a5a743894a0e4a801fc3'),
+(4, 'fahmi', 'f11d50d63d3891a44c332e46d6d7d561');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `anggota`
+--
+ALTER TABLE `anggota`
+  ADD PRIMARY KEY (`nis`);
+
+--
+-- Indexes for table `buku`
+--
+ALTER TABLE `buku`
+  ADD PRIMARY KEY (`kode_buku`);
+
+--
+-- Indexes for table `petugas`
+--
+ALTER TABLE `petugas`
+  ADD PRIMARY KEY (`id_petugas`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `petugas`
+--
+ALTER TABLE `petugas`
+  MODIFY `id_petugas` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
