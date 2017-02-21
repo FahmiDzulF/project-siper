67cdcd5a916864306b38709f827cda6d119eca16
fahmidzulf <fahmifikri13@gmail.com>
2017-02-21 15:53:19 +0700

    Models & Views Buku

--- application/views/buku/index.php
+++ application/views/buku/index.php
@@ -10,6 +10,29 @@
 
   <div class="nav navbar-nav navbar-right">
     <form class="navbar-form navbar-left" role="search" action="<?php echo site_url('buku/cari');?>" method="post">
-      <div class="">
+      <div class="form-group">
+        <label>Cari Kode / Judul</label>
+        <input type="text" class="form-control" placeholder="Search" name="cari">
+      </div>
+      <button type="submit" class="btn btn-default">
+      </form>
+    </div>
+    <a href="<?php echo site_url('buku/tambah');?>" class="btn btn-primary"><i class="glyphicon glyphicon-plus"></i> Tambah</a>
+    <hr>
+
+    <?php echo $mesage;?>
+    <table class="table table-striped">
+      <thead>
+        <tr>
+        <td>No. </td>
+        <td>Image</td>
+        <td>Kode BuKU</td>
+        <td>Judul</td>
+        <td>Pengarang</td>
+        <td>Klasifikasi</td>
+        <td colspan="2"></td>
+      </tr>
+    </thead>
+
 
 </html>
